84fe7bf94502810332e3cfa92dc5d72ce752f75b
Willem Veerman <willem.veerman@digital.homeoffice.gov.uk>
2021-03-01 22:01:41 +0000

    update python

--- lambda/aws-health-notifier.py
+++ lambda/aws-health-notifier.py
@@ -1,11 +1,111 @@
 #!/usr/bin/env python
-import json
 import boto3
+import os
+import logging
 
-def get_ns(event):
-    event['detail']
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def get_services(event, session):
+
+    services = []
+
+    try:
+        affected_entities = event['detail']['affectedEntities']
+    except KeyError as e:
+        logging.error(f"Cannot get ns, {e} element missing from event")
+        return services
+
+    tags = {}
+
+    tag_client = session.client('resourcegroupstaggingapi', region_name=os.environ['AWS_REGION'])
+
+    for entity in affected_entities:
+        try:
+            tags = entity['tags']
+            logging.info(f"Found tags for {entity}: {tags}")
+        except KeyError as e:
+            logging.error(f"{e} not found for {entity} - searching manually")
+            if entity[:4]['entityValue'] == 'arn:':
+                resource_details = tag_client.get_resources(ResourceARNList=[entity['entityValue']])
+                tags = resource_details['ResourceTagMappingList'][0]['Tags']
+                logging.info(f"Found tags for {entity}: {tags}")
+            else:
+                logging.error(f"{entity['entityValue']} not in ARN format, skipping tag retrieval")
+                return
+
+        project_service = ''
+
+        if type(tags) is list:
+            for tag in tags:
+                if tag['Key'] == 'PROJECT-SERVICE':
+                    project_service = tag['Value']
+            if not project_service:
+                logging.error(f"{entity} has no 'PROJECT-SERVICE' tag, found {tags}")
+        if type(tags) is dict:
+            try:
+                project_service = tags['PROJECT-SERVICE']
+            except KeyError as e:
+                logging.error(f"{entity} has no {e} tag, found {tags}")
+
+        if project_service:
+            logging.info(f"Found service for {entity}: {project_service}")
+            services.append(project_service)
+
+        return services
+
+def get_topics(sns_client, topics, region, next_token=''):
+
+    topic_response = sns_client.list_topics(NextToken=next_token)
+
+    if topic_response['Topics']:
+        for topic in topic_response['Topics']:
+            prefix_string = f"arn:aws:sns:{region}:{os.environ['AWS_ACCOUNT_ID']}:acp_health_alert_"
+            prefix_length = len(prefix_string)
+            if topic['TopicArn'][:prefix_length] == prefix_string:
+                logging.info(f"Found ACP Health topic: {topic['TopicArn']}")
+                topics[topic['TopicArn'][prefix_length:prefix_length]] = topic['TopicArn']
+
+    if topic_response['NextToken']:
+        get_topics(sns_client, topics, topic_response['NextToken'])
+    else:
+        return topics
+
+def parse_services(topics, project_service):
+
+    try:
+        topic_arn = topics[project_service]
+    except KeyError as e:
+        message = f"{e} not found - total SNS topics found: {[list(topics.keys())]}"
+        logging.error(message)
+        return
+
+    return topic_arn
+
+
+def trigger_sns(topic, sns_client):
+
+    sns_client.publish(TopicArn=topic)
 
 def main(event, context):
-    print(type(event))
-    print(event)
-    print(context)
+
+    aws_region = os.environ['AWS_REGION']
+
+    session = boto3.Session()
+
+    affected_services = get_services(event, session)
+
+    sns_client = session.client('sns', region_name=aws_region)
+
+    topics = {}
+
+    all_health_topics = get_topics(sns_client, topics, aws_region)
+
+    if affected_services:
+        for service in affected_services:
+            topic_arn = parse_services(all_health_topics, service)
+            if topic_arn:
+                trigger_sns(topic_arn, sns_client)
+    else:
+        for topic_arn in all_health_topics.values():
+            trigger_sns(topic_arn, sns_client)
